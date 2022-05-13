2df95d87ec1656338e24b9c1fcfecb40e673d448
Niels Sandholt Busch <niels.busch@gmail.com>
2022-05-13 15:12:29 +0200

    Add audience and linting

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
 root.render(
   <Auth0Provider
     domain='myjobplanner.eu.auth0.com'
     clientId='VDyAPJ6AmCTn53b6JavY2fayp72sZ1Lt'
     redirectUri={window.location.origin}
+    audience='https://api.myjobplanner.com'
+    scope='openid email create:business read:business'
   >
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </Auth0Provider>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
